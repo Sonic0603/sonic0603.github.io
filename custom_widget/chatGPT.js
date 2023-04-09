2c31e418774cec9148ad4188ca7dd14925e8afb9
Sonic0603 <78787756+Sonic0603@users.noreply.github.com>
2023-04-09 21:09:59 +0800

    Update chatGPT.js

--- custom_widget/chatGPT.js
+++ custom_widget/chatGPT.js
@@ -5,7 +5,7 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "gpt-4",
+        model: "text-davinci-003",
         prompt: prompt,
         max_tokens: 1024,
         n: 1,
@@ -29,7 +29,7 @@ var ajaxCall = (key, url, prompt) => {
   });
 };
 
-const url = "https://api.openai.com/v1/chat";
+const url = "https://api.openai.com/v1";
 
 
 (function () {
